cc7e2ef6bc709edc9b7043e74ab3bd3f8c4e53d4
matheus-nicolau <matheusnicolau.dev@gmail.com>
2025-07-23 20:23:43 -0400

    first commit.

---
+++ src/main/java/github/matheus_nicolau/core_clients/CoreClientsApplication.java
@@ -0,0 +1,13 @@
+package github.matheus_nicolau.core_clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreClientsApplication.class, args);
+	}
+
+}
